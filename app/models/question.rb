5be4467bba9cf0e5e6c0f86bd5c4bbf38813d2d0
Manh Vu <vdmanh107@gmail.com>
2018-08-10 11:04:12 +0700

    add model
    --amend

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,6 +4,9 @@ class Question < ApplicationRecord
   has_many :answers
   has_many :results
   has_many :lessons, through: :results
+<<<<<<< HEAD
 
   enum multi_correct: {no: 0, yes: 1}
+=======
+>>>>>>> add model
 end
