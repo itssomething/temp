3a91db41e1ad4f29f692149368d176a517e4180b
Manh Vu <vdmanh107@gmail.com>
2018-08-14 11:06:09 +0700

    temp

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,18 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+
+$(document).on("turbolinks:load", function() {
+  $("form").on("click", ".add_fields", function(event) {
+   var regexp, time;
+   time = new Date().getTime();
+   regexp = new RegExp($(this).data("id"), "g");
+   $(".fields").append($(this).data("fields").replace(regexp, time));
+   return event.preventDefault() ;
+ });
+  $("form").on("click", ".remove_record", function(event) {
+   $(this).prev("input[type=hidden]").val("1");
+   $(this).closest("div.step").hide();
+   return event.preventDefault() ;
+  });
+});
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def link_to_add_row name, form, association, **args
+    new_object = form.object.send(association).klass.new
+    id = new_object.object_id
+    fields = form.fields_for(association, new_object, child_index: id) do |b|
+      render(association.to_s.singularize, f: b)
+    end
+
+    link_to(name, "#", class: "add_fields " + args[:class],
+      data: {id: id, fields: fields.delete("\n")})
+  end
 end
